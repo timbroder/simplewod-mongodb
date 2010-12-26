b4c4ef5eaf74c40cd44ea3626221147677e62fcc
broderboy <none@none>
2010-12-26 17:32:38 -0500

    fixed some local stuff

--- settings.py
+++ settings.py
@@ -16,11 +16,6 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
-try:
-    from local_settings import *
-except ImportError:
-    pass
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -92,3 +87,8 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.core.context_processors.auth",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
 )
+
+#try:
+from local_settings import *
+#except ImportError:
+#pass
