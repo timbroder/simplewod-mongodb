5034858e5045e6ca5667c842fa79737ef7ba4962
broderboy <none@none>
2011-01-24 21:17:51 -0500

    json stuff for measure

--- mongo/models.py
+++ mongo/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core import serializers
+import simplejson
 
 # Create your models here.
 class Measure(models.Model):
@@ -7,9 +9,27 @@ class Measure(models.Model):
     def __unicode__(self):
         return self.name
     
+    class Meta:
+        ordering = ['name']
+    
 class MeasureType(models.Model):
     name = models.CharField(max_length=128)
     measures = models.ManyToManyField(Measure)
+    django_id = models.IntegerField(blank=True, null=True)
     
     def __unicode__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.django_id = 0
+        super(MeasureType,self).save(*args, **kwargs)
+        self.django_id = self.id
+        print self.django_id
+        json =  serializers.serialize('json', [self])[1:-1]
+        #print json
+        json = simplejson.loads(json)
+        #print json
+        #print object_hook
+        #print as_complex
+        return json
