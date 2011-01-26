80baa38b17802c402a965f8b3407d052aa381fc5
broderboy <none@none>
2011-01-25 19:50:06 -0500

    model stuff.  less json in excersize then i thought there would be

--- mongo/models.py
+++ mongo/models.py
@@ -15,21 +15,34 @@ class Measure(models.Model):
 class MeasureType(models.Model):
     name = models.CharField(max_length=128)
     measures = models.ManyToManyField(Measure)
-    django_id = models.IntegerField(blank=True, null=True)
+    #django_id = models.IntegerField(blank=True, null=True)
     
     def __unicode__(self):
         return self.name
     
     def save(self, *args, **kwargs):
-        if not self.id:
-            self.django_id = 0
-        super(MeasureType,self).save(*args, **kwargs)
-        self.django_id = self.id
-        print self.django_id
-        json =  serializers.serialize('json', [self])[1:-1]
+        #if not self.id:
+        #    self.django_id = 0
+        return super(MeasureType,self).save(*args, **kwargs)
+        #self.django_id = self.id
+        #print self.django_id
+        #json =  serializers.serialize('json', [self])[1:-1]
         #print json
-        json = simplejson.loads(json)
+        #json = simplejson.loads(json)
         #print json
         #print object_hook
         #print as_complex
-        return json
+        #return json
+        
+class Exercise(models.Model):
+    name = models.CharField(max_length=128)
+    type = models.ForeignKey(MeasureType)
+    
+    #full json 
+    #serializers.serialize('json', [Exercise.objects.all()[0]], indent=4, relations={'type': {'relations': ('measures',)}})
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
