916e457000e7d834258ac080a2894d095e386f8d
broderboy <none@none>
2011-02-21 14:21:42 -0500

    require.js

--- static/js/base.js
+++ static/js/base.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
 	//Cufon.replace('#logowod', { fontFamily: 'Amsterdam Graffiti' });
-	Cufon.replace('#logowod', { fontFamily: 'Octin Spraypaint Free' });
+	//Cufon.replace('#logowod', { fontFamily: 'Octin Spraypaint Free' });
 });	
